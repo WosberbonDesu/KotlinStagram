0ebec6c8645b87cb01cfd1761104117657883c08
Berke Can Pınar <69467096+WosberbonDesu@users.noreply.github.com>
2021-10-19 13:28:39 +0300

    code added
    code added

---
+++ kotlininstagram/app/src/main/java/com/berkecanpinar/kotlininstagram/model/Post.kt
@@ -0,0 +1,4 @@
+package com.berkecanpinar.kotlininstagram.model
+
+class Post(val email: String, val comment: String,val downloadUrl : String) {
+}
---
+++ kotlininstagram/app/src/main/java/com/berkecanpinar/kotlininstagram/view/FeedActivity.kt
@@ -0,0 +1,80 @@
+package com.berkecanpinar.kotlininstagram.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.berkecanpinar.kotlininstagram.R
+import com.berkecanpinar.kotlininstagram.databinding.ActivityFeedBinding
+import com.berkecanpinar.kotlininstagram.model.Post
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class FeedActivity : AppCompatActivity() {
+
+    private lateinit var  binding : ActivityFeedBinding
+    private lateinit var auth : FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+    private lateinit var postArrayList: ArrayList<Post>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFeedBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        auth = Firebase.auth
+        db = Firebase.firestore
+
+        postArrayList = ArrayList<Post>()
+        getData()
+    }
+
+    private fun getData(){
+
+        db.collection("Posts").addSnapshotListener { value, error ->
+            if(error != null){
+                Toast.makeText(this,error.localizedMessage,Toast.LENGTH_LONG).show()
+            }else{
+                if(value != null){
+                     if(!value.isEmpty){
+                         val documents = value.documents
+                         for(document in documents){
+                             val comment = document.get("comment") as String
+                             val userEmail = document.get("userEmail") as String
+                             val downloadUrl = document.get("downloadUrl") as String
+                             val post = Post(userEmail,comment,downloadUrl)
+                             postArrayList.add(post)
+                         }
+                     }
+                }
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.insta_menu,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == R.id.add_post){
+            val intent = Intent(this, UploadActivity::class.java)
+            startActivity(intent)
+        }else if(item.itemId == R.id.signout){
+            auth.signOut()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
